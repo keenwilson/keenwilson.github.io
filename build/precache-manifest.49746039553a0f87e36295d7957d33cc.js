3a6f944e25029f0c39c3974f21cb0f5bed055382
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2019-02-04 19:44:26 -0600

    Edit footer

---
+++ build/precache-manifest.49746039553a0f87e36295d7957d33cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "950d258be65b347a37ad",
+    "url": "/static/css/main.6bb9a881.chunk.css"
+  },
+  {
+    "revision": "950d258be65b347a37ad",
+    "url": "/static/js/main.950d258b.chunk.js"
+  },
+  {
+    "revision": "2a2e7091245be66b16d7",
+    "url": "/static/js/1.2a2e7091.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cbb1f11e21f026f5ed1045a6893b44cb",
+    "url": "/index.html"
+  }
+];
--- src/components/heroFooter.js
+++ src/components/heroFooter.js
@@ -4,7 +4,7 @@ const HeroFooter = () => {
   return (
     <div className="container has-text-centered">
       <h3 className="subtitle has-text-weight-light text-center">
-        My recent works could be found down here !
+        My recent work can be found down here !
         <i className="far fa-hand-point-down hvr-grow" />
       </h3>
     </div>
